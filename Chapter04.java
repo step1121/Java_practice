f6545dc66e78046cebc557ee7b8f39d4e6b36788
hoshi yuki <au5.iphonex3@outlook.jp>
2022-09-26 11:33:15 +0900

    Chapter04

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String str = "Yuki Hoshi";
+        System.out.println(str);
+        str = "慈悲";
+        System.out.println(str);
+        final int LUCKY_NUMBER = 7;
+        System.out.println(LUCKY_NUMBER);
+    }
+}
